1821f9367dbac457e5e86516d677b42dcdf6b14f
saurav-bitkraft <saurav.bitkraft@outlook.com>
2024-07-20 22:42:37 +0530

    added the formgroup and removed individual FormControl for each field in the contact form

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,20 +1,25 @@
 import { Component } from '@angular/core';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, ReactiveFormsModule, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
   imports: [ReactiveFormsModule],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrl: './contact.component.css',
 })
 export class ContactComponent {
-senderNameControl= new FormControl('Saurav');
-senderEmailControl= new FormControl('');
-senderMessageControl= new FormControl('');
-submitForm(){
-  if(this.senderNameControl.dirty){
-    alert(`you changed ${this.senderNameControl.value} name field`)
+
+  contactForm= new FormGroup({
+
+    senderName : new FormControl('Saurav'),
+    senderEmail : new FormControl(''),
+    senderMessage : new FormControl('')
+  })
+  submitForm() {
+    console.log(this.contactForm.value)
+    // if (this.senderNameControl.dirty) {
+    //   alert(`you changed ${this.senderNameControl.value} name field`);
+    // }
   }
-}
 }
